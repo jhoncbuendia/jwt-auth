6fdeac3d56600a363b248dee963647a1db31008d
jhon.buendia <jhon.buendia@globant.com>
2016-03-28 15:08:50 -0500

    authentication token service started

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var auth = require('../services/authentication/authentication.js')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/:username/:pwd', function(req, res, next) {
+  auth.createToken(req.params.username, req.params.pwd).then(
+    function(data){
+      res.json(data);
+    },
+    function(err){
+      res.json(err);
+    }
+  );
 });
 
 module.exports = router;
---
+++ server/services/authentication/authentication.js
@@ -0,0 +1,71 @@
+/*
+  links:
+    https://carlosazaustre.es/blog/autenticacion-con-token-en-node-js/
+  tasks:
+    implement the service for save the users using md5 encryption
+    implement authentication token service finding the users in the database
+    implement authorization service based in users roles
+    implement authorization middleware for get access to api
+    
+*/
+
+
+
+var mongoose = require('mongoose');
+var jwt = require('jwt-simple');
+var moment = require('moment');
+
+
+mongoose.connect('mongodb://localhost/auth');
+var uniqueValidator = require('mongoose-unique-validator');
+
+
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+  username:  {type:String,  unique: true},
+  pwd: String,
+});
+
+userSchema.plugin(uniqueValidator);
+
+module.exports = {
+  createToken : function(username, pwd){
+
+  var User = mongoose.model('User', userSchema);
+
+  User.findOne({ 'username': 'jhon' }, function (err, user) {
+      if (err){console.log(err);}
+      else{console.log(user.username);}
+  });
+
+    var payload = {
+      username: username,
+      pwd: pwd,
+      role_id: 1,
+      iat: moment().unix(),
+      exp: moment().add(14, "days").unix(),
+    };
+
+    var secret = 'xxx';
+    // encode
+    var token = jwt.encode(payload, secret);
+    //console.log(token); //=> { foo: 'bar' }
+    // decode
+    var decoded = jwt.decode(token, secret);
+    //console.log(decoded); //=> { foo: 'bar' }
+
+    var promise = new Promise(function(fullfil, reject){
+
+      var user = new User({username: "jhon", pwd: "123456"})
+
+      user.save(function (err) {
+      if(err){reject(err);}
+      else{fullfil({status: 200});}
+      });
+
+    });
+
+    return promise;
+  }
+};
